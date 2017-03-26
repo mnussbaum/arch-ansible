3518d5bbfeba5fc4726b9f0714b9df898528aa32
mnussbaum <michaelnussbaum08@gmail.com>
2017-03-26 15:03:38 -0700

    Improve firefox perf

--- files/firefox-user.js
+++ files/firefox-user.js
@@ -1,2 +1,19 @@
+// High DPI
 user_pref("layout.css.devPixelsPerPx", "2");
+
+// Hardware accelerate with OpenGL
 user_pref("layers.acceleration.force-enabled", true);
+
+// Enable pipelining
+user_pref("network.http.pipelining", true);
+user_pref("network.http.pipelining.ssl", true);
+user_pref("network.http.proxy.pipelining", true);
+
+// Enable HTTP cache
+user_pref("browser.cache.use_new_backend", 1);
+
+// Decrease disk writes, less frequent session restore writes
+user_pref("browser.sessionstore.interval", 300000);
+
+// Make pages render immediately
+user_pref("nglayout.initialpaint.delay", 0);
