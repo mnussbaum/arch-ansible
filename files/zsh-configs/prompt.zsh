b657f02938419ea134aa7a51049175c5be5dd854
mnussbaum <michaelnussbaum08@gmail.com>
2018-06-22 16:11:58 -0700

    Add spaceship prompt

--- files/zsh-configs/prompt.zsh
+++ files/zsh-configs/prompt.zsh
@@ -1,22 +1,4 @@
-truncated_pwd() {
-  echo '%(5~|%-1~/.../%3~|%4~)'
-}
+export SPACESHIP_VI_MODE_SHOW="false"
 
-git_prompt() {
-  prompt=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-  branch_rc=$?
-
-  if [ $branch_rc -eq 0 ] ; then
-    git diff-index --quiet HEAD -- 2>&1 >/dev/null
-    dirty_rc=$?
-
-    if ! [ $dirty_rc -eq 0 ] ; then
-      prompt="${prompt}%F{$bright_red}!%f"
-    fi
-
-    echo "%F{$purple}[$prompt%F{$purple}]%f "
-  fi
-}
-
-setopt prompt_subst
-export PROMPT='%F{$aqua}$(truncated_pwd)%f $(git_prompt)%F{$bright_yellow}>%F{$bright_green}>%F{$red}>%f '
+autoload -U promptinit; promptinit
+prompt spaceship
