0eae7517aac34b05669664944a2443da2faebf37
mnussbaum <michaelnussbaum08@gmail.com>
2019-06-11 19:46:33 -0700

    Add color to zsh prompt
    And make it show the exit code of a failed command

--- files/zsh-configs/prompt.zsh
+++ files/zsh-configs/prompt.zsh
@@ -1,3 +1,7 @@
+export SPACESHIP_CHAR_SYMBOL="%{\e[38;5;001m%}‣%{\e[38;5;011m%}‣%{\e[38;5;004m%}‣ "
+# Yields three arrows, colored red, yellow and blue
+
+export SPACESHIP_EXIT_CODE_SHOW="true"
 export SPACESHIP_VI_MODE_SHOW="false"
 
 autoload -U promptinit; promptinit
