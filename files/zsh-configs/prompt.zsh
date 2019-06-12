bae5ac8e759a75599c531f85a5fae37816e42163
mnussbaum <michaelnussbaum08@gmail.com>
2019-06-11 23:12:54 -0700

    Use bullets instead of arrows in zsh prompt

--- files/zsh-configs/prompt.zsh
+++ files/zsh-configs/prompt.zsh
@@ -1,5 +1,5 @@
-export SPACESHIP_CHAR_SYMBOL="%{\e[38;5;001m%}‣%{\e[38;5;011m%}‣%{\e[38;5;004m%}‣ "
-# Yields three arrows, colored red, yellow and blue
+export SPACESHIP_CHAR_SYMBOL="%{\e[38;5;001m%}•%{\e[38;5;011m%}•%{\e[38;5;004m%}• "
+# Yields three bullets, colored red, yellow and blue
 
 export SPACESHIP_EXIT_CODE_SHOW="true"
 export SPACESHIP_VI_MODE_SHOW="false"
