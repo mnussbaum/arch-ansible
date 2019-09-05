16e2d1c4a0f269aa9d6113241f8720e5ddecb341
mnussbaum <michaelnussbaum08@gmail.com>
2019-09-05 00:38:49 -0700

    Add virtual keyboard toggle to bar. Add bottom waybat
    The bottom bar has toggles on the left and should eventually have real
    world-ish info like weather and music

--- filter_plugins/waybar_colored_span.py
+++ filter_plugins/waybar_colored_span.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 
+
 class FilterModule(object):
     def filters(self):
-        return {
-            "waybar_colored_block": self.waybar_colored_block
-        }
+        return {"waybar_colored_block": self.waybar_colored_block}
 
     def waybar_colored_block(
         self,
@@ -14,15 +13,28 @@ class FilterModule(object):
         text,
         first_block=False,
         last_block=False,
+        position=None,
     ):
-        if first_block:
-            color = color_scheme_vars[color_base]
-            return f'''<span color=\\"#{color}\\"></span><span background=\\"#{color}\\"> {text}</span>'''
-
-        left_color = color_scheme_vars[left_color_base]
         color = color_scheme_vars[color_base]
 
-        if last_block:
-            return f'''<span background=\\"#{left_color}\\" color=\\"#{left_color}\\"></span><span background=\\"#{left_color}\\" color=\\"#{color}\\"></span><span background=\\"#{color}\\"> {text} </span>'''
+        if first_block and position == "right":
+            return f"""<span color=\\"#{color}\\"></span><span background=\\"#{color}\\"> {text}</span>"""
+        elif first_block and position == "left":
+            return f"""<span background=\\"#{color}\\"> {text}</span>"""
+        elif first_block:
+            raise ValueError(
+                f"""If last_block is True a position of 'left' or 'right' must be passed. Given: {position}"""
+            )
 
-        return f'''<span background=\\"#{left_color}\\" color=\\"#{left_color}\\"></span><span background=\\"#{left_color}\\" color=\\"#{color}\\"></span><span background=\\"#{color}\\"> {text}</span>'''
+        left_color = color_scheme_vars[left_color_base]
+
+        if last_block and position == "left":
+            return f"""<span background=\\"#{left_color}\\" color=\\"#{left_color}\\"></span><span background=\\"#{left_color}\\" color=\\"#{color}\\"></span><span background=\\"#{color}\\"> {text} </span><span color=\\"#{color}\\"></span>"""
+        elif last_block and position == "right":
+            return f"""<span background=\\"#{left_color}\\" color=\\"#{left_color}\\"></span><span background=\\"#{left_color}\\" color=\\"#{color}\\"></span><span background=\\"#{color}\\"> {text} </span>"""
+        elif last_block:
+            raise ValueError(
+                f"""If last_block is True a position of 'left' or 'right' must be passed. Given: {position}"""
+            )
+
+        return f"""<span background=\\"#{left_color}\\" color=\\"#{left_color}\\"></span><span background=\\"#{left_color}\\" color=\\"#{color}\\"></span><span background=\\"#{color}\\"> {text}</span>"""
