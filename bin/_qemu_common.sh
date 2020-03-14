7d1c64573b2c4992a03cdd944f073e76565ea74c
mnussbaum <michaelnussbaum08@gmail.com>
2020-03-13 18:35:23 -0700

    Set args necessary for qemu guest agent

--- bin/_qemu_common.sh
+++ bin/_qemu_common.sh
@@ -8,6 +8,9 @@ qemu_install_iso="$qemu_data_dir/live-usb.iso"
 qemu_ovmf_vars_file="$qemu_data_dir/uefi-vars.bin"
 
 common_qemu_args=(
+  "-chardev" "socket,path=/tmp/qga.sock,server,nowait,id=qga0" \
+  "-device" "virtio-serial" \
+  "-device" "virtserialport,chardev=qga0,name=org.qemu.guest_agent.0" \
   "-drive" "if=pflash,format=raw,readonly,file=/usr/share/ovmf/x64/OVMF_CODE.fd" \
   "-drive" "if=pflash,format=raw,file=$qemu_ovmf_vars_file" \
   "-m" "4096" \
