3f8c0e9ec98355e35d58900c030f55d7744478b8
mnussbaum <michaelnussbaum08@gmail.com>
2021-03-08 21:48:40 -0800

    Add qemu options from run_archiso script
    Hopefully this improves perf a bit. Pulls options from -
    https://gitlab.archlinux.org/archlinux/archiso/-/blob/master/scripts/run_archiso.sh

--- bin/_qemu_common.sh
+++ bin/_qemu_common.sh
@@ -8,17 +8,25 @@ qemu_install_iso="$qemu_data_dir/live-usb.iso"
 qemu_ovmf_vars_file="$qemu_data_dir/uefi-vars.bin"
 
 common_qemu_args=(
-  "-chardev" "socket,path=/tmp/qga.sock,server,nowait,id=qga0" \
-  "-device" "virtio-serial" \
-  "-device" "virtserialport,chardev=qga0,name=org.qemu.guest_agent.0" \
-  "-drive" "if=pflash,format=raw,readonly,file=/usr/share/ovmf/x64/OVMF_CODE.fd" \
-  "-drive" "if=pflash,format=raw,file=$qemu_ovmf_vars_file" \
-  "-m" "4096" \
-  "-enable-kvm" \
-  "-M" "q35" \
-  "-cpu" "host" \
-  "-nic" "user,model=virtio-net-pci" \
-  "-smp" "4,sockets=1,cores=4,threads=1" \
   "-bios" "/usr/share/qemu/bios.bin" \
   "-boot" "menu=on" \
+  "-chardev" "socket,path=/tmp/qga.sock,server,nowait,id=qga0" \
+  "-cpu" "host" \
+  "-enable-kvm" \
+  "-device" "virtio-rng-pci" \
+  "-device" "virtio-serial" \
+  "-device" "virtserialport,chardev=qga0,name=org.qemu.guest_agent.0" \
+  "-display" "sdl,gl=on" \
+  "-drive" "if=pflash,format=raw,readonly,file=/usr/share/ovmf/x64/OVMF_CODE.fd" \
+  "-drive" "if=pflash,format=raw,file=$qemu_ovmf_vars_file" \
+  "-global" "ICH9-LPC.disable_s3=1" \
+  "-m" "4096" \
+  "-machine" "type=q35,smm=on,accel=kvm,usb=on" \
+  "-monitor" "none" \
+  "-name" "archiso,process=archiso_0" \
+  "-nic" "user,model=virtio-net-pci" \
+  "-parallel" "none" \
+  "-serial" "stdio" \
+  "-smp" "4,sockets=1,cores=4,threads=1" \
+  "-vga" "virtio"
 )
