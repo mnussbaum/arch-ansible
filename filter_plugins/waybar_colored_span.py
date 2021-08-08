f4e8c873cefae95a480eeaad3917c547ae590322
mnussbaum <michaelnussbaum08@gmail.com>
2021-08-07 22:04:28 -0700

    Fix waybar glyph sizing issues under Cascadia
    * Use an invisible character to fix wide glyph truncation
    * Large waybar font size normalizes weird triangle glyph heights

--- filter_plugins/waybar_colored_span.py
+++ filter_plugins/waybar_colored_span.py
@@ -18,7 +18,7 @@ class FilterModule(object):
         color = color_scheme_vars[color_base]
 
         if first_block and position == "right":
-            return f"""<span color=\\"#{color}\\"></span><span background=\\"#{color}\\"> {text}</span>"""
+            return f"""<span size=\\"large\\" color=\\"#{color}\\"></span><span background=\\"#{color}\\"> {text}</span>"""
         elif first_block and position == "left":
             return f"""<span background=\\"#{color}\\"> {text}</span>"""
         elif first_block:
@@ -29,12 +29,12 @@ class FilterModule(object):
         left_color = color_scheme_vars[left_color_base]
 
         if last_block and position == "left":
-            return f"""<span background=\\"#{left_color}\\" color=\\"#{left_color}\\"></span><span background=\\"#{left_color}\\" color=\\"#{color}\\"></span><span background=\\"#{color}\\"> {text} </span><span color=\\"#{color}\\"></span>"""
+            return f"""<span size=\\"large\\" background=\\"#{left_color}\\" color=\\"#{left_color}\\"></span><span size=\\"large\\" background=\\"#{left_color}\\" color=\\"#{color}\\"></span><span background=\\"#{color}\\"> {text} </span><span size=\\"large\\" color=\\"#{color}\\"></span>"""
         elif last_block and position == "right":
-            return f"""<span background=\\"#{left_color}\\" color=\\"#{left_color}\\"></span><span background=\\"#{left_color}\\" color=\\"#{color}\\"></span><span background=\\"#{color}\\"> {text} </span>"""
+            return f"""<span size=\\"large\\" background=\\"#{left_color}\\" color=\\"#{left_color}\\"></span><span size=\\"large\\" background=\\"#{left_color}\\" color=\\"#{color}\\"></span><span background=\\"#{color}\\"> {text} </span>"""
         elif last_block:
             raise ValueError(
                 f"""If last_block is True a position of 'left' or 'right' must be passed. Given: {position}"""
             )
 
-        return f"""<span background=\\"#{left_color}\\" color=\\"#{left_color}\\"></span><span background=\\"#{left_color}\\" color=\\"#{color}\\"></span><span background=\\"#{color}\\"> {text}</span>"""
+        return f"""<span size=\\"large\\" background=\\"#{left_color}\\" color=\\"#{left_color}\\"></span><span size=\\"large\\" background=\\"#{left_color}\\" color=\\"#{color}\\"></span><span background=\\"#{color}\\"> {text}</span>"""
--- fonts-todo.md
+++ fonts-todo.md
@@ -1,11 +1,5 @@
-x Move to Cascadia Code
 x Freeze in place italics with a cursive variant
   - Used python-opentype-feature-freezer
-* Fix the cut off symbols
-  - https://github.com/ryanoasis/nerd-fonts/issues/442
-  - https://github.com/microsoft/cascadia-code/issues/544
-  - Opened a waybar issue - https://github.com/Alexays/Waybar/issues/1185
-* Make triangles bigger
-* Fix the waybar clock being too small
+  - Need to automate the generation and install of the patched font still
 * Make vim use italics for comments
   - https://rsapkf.xyz/blog/enabling-italics-vim-tmux
