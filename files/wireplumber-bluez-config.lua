fa5bfeb296175ab115fbf649468c9faf1e518720
mnussbaum <michaelnussbaum08@gmail.com>
2022-07-23 16:57:36 -0700

    Replace pipewire-media-session with wireplumber
    Can't get smbc high quality audio working though

---
+++ files/wireplumber-bluez-config.lua
@@ -0,0 +1,7 @@
+bluez_monitor.properties = {
+  ["bluez5.enable-msbc"] = true,
+  ["bluez5.enable-hw-volume"] = true,
+  ["bluez5.headset-roles"] = "[hfp_hf hsp_hs]",
+  ["bluez5.codecs"] = "[sbc]"
+  ["with-logind"] = true,
+}
