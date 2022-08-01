20615034e6aeb3484a85d8ad95747fe466f5ab1f
mnussbaum <michaelnussbaum08@gmail.com>
2022-07-31 18:44:54 -0700

    Formatters working, need format on save still

--- files/nvim/lua/trailing-whitespace.lua
+++
@@ -1,9 +0,0 @@
--- Highlight trailing whitespace
-
-vim.api.nvim_exec([[
-  highlight ExtraWhitespace ctermbg=red guibg=red
-  match ExtraWhitespace /\s\+$/
-  autocmd BufWinEnter * match ExtraWhitespace /\s\+$/
-  autocmd InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$/
-  autocmd InsertLeave * match ExtraWhitespace /\s\+$/
-]], false)
---
+++ luaify.md
@@ -0,0 +1,9 @@
+TODO:
+
+* Intelligent text objects
+* Diagnostics and fixits - figure how to use them
+* Enable format on save
+* Completions
+* vim-vista integration
+* fancy go plugin
+* Telescope
