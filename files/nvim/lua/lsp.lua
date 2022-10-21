a181f819323353c9ca78101a4813ed58c510de42
mnussbaum <michaelnussbaum08@gmail.com>
2022-10-20 17:29:32 -0700

    Fix nerdtree leaving pane open. Fix deprecated vim plugin setting

--- files/nvim/lua/lsp.lua
+++ files/nvim/lua/lsp.lua
@@ -79,6 +79,6 @@ for _, language_server in ipairs(require("language_servers")) do
 		on_attach = on_attach,
 
 		-- Pull completions from LSP
-		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+		capabilities = require("cmp_nvim_lsp").default_capabilities(),
 	})
 end
--- files/nvim/lua/nerdtree.lua
+++ files/nvim/lua/nerdtree.lua
@@ -1,13 +1,4 @@
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>nt",
-  ":NERDTreeToggle<CR>",
-  { silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>nf",
-  ":NERDTreeFind<CR>",
-  { silent = true }
-)
+vim.api.nvim_set_keymap("n", "<leader>nt", ":NERDTreeToggle<CR>", { silent = true })
+vim.api.nvim_set_keymap("n", "<leader>nf", ":NERDTreeFind<CR>", { silent = true })
 vim.g.NERDTreeShowHidden = 1
+vim.g.NERDTreeMinimalMenu = 1
