6fa02cb8b6bea6e1cacf451dbe25b0a878a988b0
mnussbaum <michaelnussbaum08@gmail.com>
2022-12-24 13:22:48 -0500

    Fix insert delay. Fix treesitter plugin init
    Treesitter plugins weren't being initialized due to overriding calls to
    the setup function. Also, the tree climber plugin was creating a
    significant delay before entering insert mode

--- files/nvim/lua/tree-sitter.lua
+++ files/nvim/lua/tree-sitter.lua
@@ -4,4 +4,24 @@ require("nvim-treesitter.configs").setup({
 	auto_install = true,
 
 	highlight = { enable = true },
+	textobjects = {
+		swap = {
+			enable = true,
+			swap_next = {
+				["ta"] = "@parameter.inner",
+			},
+			swap_previous = {
+				["tA"] = "@parameter.inner",
+			},
+		},
+	},
+	textsubjects = {
+		enable = true,
+		prev_selection = ",",
+		keymaps = {
+			["."] = "textsubjects-smart",
+			[";"] = "textsubjects-container-outer",
+			["i;"] = "textsubjects-container-inner",
+		},
+	},
 })
