8d70bee5dd446cc4c0c6a9048717ba1da32b2d11
mnussbaum <michaelnussbaum08@gmail.com>
2023-02-17 19:09:16 -0800

    Fix ansible bootstrap command args

--- bin/_bootstrap_common.sh
+++ bin/_bootstrap_common.sh
@@ -8,8 +8,8 @@ bootstrap() {
   NO_ASK_BECOME_PASS=1 \
     ANSIBLE_PLAYBOOK=bootstrap.yml \
     exec ./bin/ansible \
-    --extra-args new_hostname=$HOSTNAME \
-    --extra-args bootstrap="true" \
+    --extra-vars new_hostname=$HOSTNAME \
+    --extra-vars bootstrap="true" \
     --tags bootstrap \
     $@
 }
