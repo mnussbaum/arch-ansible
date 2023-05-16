4cfb80f2fe01bb82c7c8bbc387ac64a971c1fb33
mnussbaum <michaelnussbaum08@gmail.com>
2023-05-16 01:58:33 -0700

    Update for deprecated qemu args

--- bin/_qemu_common.sh
+++ bin/_qemu_common.sh
@@ -10,12 +10,13 @@ qemu_ovmf_vars_file="$qemu_data_dir/uefi-vars.bin"
 common_qemu_args=(
   "-bios" "/usr/share/qemu/bios.bin" \
   "-boot" "menu=on" \
-  "-chardev" "socket,path=/tmp/qga.sock,server,nowait,id=qga0" \
+  "-chardev" "socket,path=/tmp/qga.sock,server=on,wait=off,id=qga0" \
   "-cpu" "host" \
   "-enable-kvm" \
   "-device" "virtio-rng-pci" \
   "-device" "virtio-serial" \
   "-device" "virtserialport,chardev=qga0,name=org.qemu.guest_agent.0" \
+  "-nic" "user,model=virtio-net-pci" \
   "-display" "sdl,gl=on" \
   "-drive" "if=pflash,format=raw,readonly=on,file=/usr/share/ovmf/x64/OVMF_CODE.fd" \
   "-drive" "if=pflash,format=raw,file=$qemu_ovmf_vars_file" \
@@ -24,7 +25,6 @@ common_qemu_args=(
   "-machine" "type=q35,smm=on,accel=kvm,usb=on" \
   "-monitor" "none" \
   "-name" "archiso,process=archiso_0" \
-  "-nic" "user,model=virtio-net-pci" \
   "-parallel" "none" \
   "-serial" "stdio" \
   "-smp" "4,sockets=1,cores=4,threads=1" \
