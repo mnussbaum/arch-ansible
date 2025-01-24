f588a977ed5440d2fd870e3d38cf043a393e73b0
mnussbaum <michaelnussbaum08@gmail.com>
2025-01-24 11:59:22 -0800

    Decruft and optimize bluetooth configs
    This is what the arch wiki suggests now

--- files/wireplumber-bluez-config.lua
+++ files/wireplumber-bluez-config.lua
@@ -1,7 +1,9 @@
 bluez_monitor.properties = {
-  ["bluez5.enable-msbc"] = true,
-  ["bluez5.enable-hw-volume"] = true,
-  ["bluez5.headset-roles"] = "[hfp_hf hsp_hs]",
-  ["bluez5.codecs"] = "[sbc]"
-  ["with-logind"] = true,
+  bluez5.enable-sbc-xq = true
+  bluez5.enable-msbc = true
+  bluez5.codecs = [ sbc sbc_xq ]
+  bluez5.enable-msbc = true,
+  bluez5.enable-hw-volume = true
+  bluez5.headset-roles = [hfp_hf hsp_hs]
+  with-logind = true
 }
