4f7a6e483cf93cc53157b572a3116a0a11d6552d
mnussbaum <michaelnussbaum08@gmail.com>
2025-05-07 10:58:01 -0700

    Move from avante to codecompanion

---
+++ files/nvim/lua/code_companion_setup.lua
@@ -0,0 +1,20 @@
+require("codecompanion").setup({
+	strategies = {
+		chat = {
+			adapter = "anthropic",
+		},
+		inline = {
+			adapter = "anthropic",
+		},
+		cmd = {
+			adapter = "anthropic",
+		},
+	},
+})
+
+vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+-- Expand 'cc' into 'CodeCompanion' in the command line
+vim.cmd([[cab cc CodeCompanion]])
