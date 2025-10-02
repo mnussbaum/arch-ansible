60044d155a61f1712f9ab1737ba6329abf7728f8
mnussbaum <michaelnussbaum08@gmail.com>
2025-10-02 15:14:17 -0700

    Avoid clobbering nvim increment keybind

--- files/nvim/lua/code_companion_setup.lua
+++ files/nvim/lua/code_companion_setup.lua
@@ -12,7 +12,7 @@ require("codecompanion").setup({
 	},
 })
 
-vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<C-c>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
 vim.keymap.set({ "n", "v" }, "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
 vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
 
