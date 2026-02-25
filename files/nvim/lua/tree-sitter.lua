bc948d452ca405900e0789883b468c150c5d884f
mnussbaum <michaelnussbaum08@gmail.com>
2026-02-25 11:00:47 -0800

    Get onto latest treesitter plugin
    It's a big backwards incompatible rewrite

--- files/nvim/lua/tree-sitter.lua
+++ files/nvim/lua/tree-sitter.lua
@@ -1,27 +1,6 @@
-require("nvim-treesitter.configs").setup({
+require("nvim-treesitter").setup({
 	ensure_installed = require("tree_sitter_plugins"),
 	sync_install = false,
 	auto_install = true,
-
 	highlight = { enable = true },
-	textobjects = {
-		swap = {
-			enable = true,
-			swap_next = {
-				["ta"] = "@parameter.inner",
-			},
-			swap_previous = {
-				["tA"] = "@parameter.inner",
-			},
-		},
-	},
-	textsubjects = {
-		enable = true,
-		prev_selection = ",",
-		keymaps = {
-			["."] = "textsubjects-smart",
-			[";"] = "textsubjects-container-outer",
-			["i;"] = "textsubjects-container-inner",
-		},
-	},
 })
