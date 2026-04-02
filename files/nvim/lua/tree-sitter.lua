0173f4e1752f7cdf7638dbac3fcd6a4d3ef0f8a4
mnussbaum <michaelnussbaum08@gmail.com>
2026-04-02 15:52:05 -0400

    Fix nvim treesitter plugin install
    This was updated as part of the neovim 0.12 upgrade. This has been
    broken in my setup since I upgraded tree-sitter to the main branch

--- files/nvim/lua/tree-sitter.lua
+++ files/nvim/lua/tree-sitter.lua
@@ -1,6 +1,13 @@
 require("nvim-treesitter").setup({
-	ensure_installed = require("tree_sitter_plugins"),
 	sync_install = false,
 	auto_install = true,
 	highlight = { enable = true },
 })
+
+local already_installed = require("nvim-treesitter.config").get_installed()
+local to_install = vim.iter(require("tree_sitter_plugins"))
+	:filter(function(parser)
+		return not vim.tbl_contains(already_installed, parser)
+	end)
+	:totable()
+require("nvim-treesitter").install(to_install)
