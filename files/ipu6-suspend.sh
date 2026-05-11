106b147d23178790ccfa0a017bded161012c6a81
mnussbaum <michaelnussbaum08@gmail.com>
2026-05-11 11:40:16 -0700

    During suspend disable ipu6 kernel modules properly
    Disable modules that are blocking the existing module disablement

--- files/ipu6-suspend.sh
+++ files/ipu6-suspend.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 case "$1" in
     pre)
-        modprobe -r ov01a10 intel_ipu6_isys intel_ipu6
+        modprobe -r ov01a10 ivsc_csi ivsc_ace intel_ipu6_isys intel_ipu6
         ;;
     post)
         modprobe intel_ipu6
