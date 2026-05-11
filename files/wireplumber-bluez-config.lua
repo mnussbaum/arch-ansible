de8c768ee4ca8110817e5da6a2991174cb57fe01
mnussbaum <michaelnussbaum08@gmail.com>
2026-05-11 12:18:42 -0700

    Fix wireplumber headphone configs for 0.5
    The configs were for wireplumber 0.4, and were outdated. This should
    hopefully improve headphone mic audio quality

--- files/wireplumber-bluez-config.lua
+++
@@ -1,9 +0,0 @@
-bluez_monitor.properties = {
-  bluez5.enable-sbc-xq = true
-  bluez5.enable-msbc = true
-  bluez5.codecs = [ sbc sbc_xq ]
-  bluez5.enable-msbc = true,
-  bluez5.enable-hw-volume = true
-  bluez5.headset-roles = [hfp_hf hsp_hs]
-  with-logind = true
-}
