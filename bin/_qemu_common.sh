6816e5608b8bdd2b51a8c6d33b7295c598704e37
mnussbaum <michaelnussbaum08@gmail.com>
2026-05-13 23:25:17 -0700

    Update QEMU image build for latest OVMF location

--- bin/_qemu_common.sh
+++ bin/_qemu_common.sh
@@ -18,7 +18,7 @@ common_qemu_args=(
   "-device" "virtserialport,chardev=qga0,name=org.qemu.guest_agent.0" \
   "-nic" "user,model=virtio-net-pci" \
   "-display" "sdl,gl=on" \
-  "-drive" "if=pflash,format=raw,readonly=on,file=/usr/share/ovmf/x64/OVMF_CODE.fd" \
+  "-drive" "if=pflash,format=raw,readonly=on,file=/usr/share/edk2/x64/OVMF_CODE.4m.fd" \
   "-drive" "if=pflash,format=raw,file=$qemu_ovmf_vars_file" \
   "-global" "ICH9-LPC.disable_s3=1" \
   "-m" "4096" \
