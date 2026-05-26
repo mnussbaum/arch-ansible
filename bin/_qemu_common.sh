cf76414c3471315b39c2c2a0d42903c7f26035fa
mnussbaum <michaelnussbaum08@gmail.com>
2026-05-26 15:05:29 -0700

    Got into sway. Needed some manual intervention, hopefully encoded
    Now need to fix things not finding configs

--- bin/_qemu_common.sh
+++ bin/_qemu_common.sh
@@ -7,22 +7,3 @@ qemu_output_dir=~/.cache/mkosi/images/qemu
 
 mkdir -p "$qemu_shared_dir"
 
-# Forwards the host pcscd socket into the VM over vsock so the guest's
-# scdaemon (and thus gpg-agent + SSH auth) can reach the YubiKey without
-# USB passthrough. Requires the vhost_vsock kernel module on the host.
-_pcscd_vsock_relay_pid=""
-
-start_pcscd_vsock_relay() {
-  local pcscd_sock=/run/pcscd/pcscd.comm
-  if [[ -S "$pcscd_sock" ]]; then
-    socat VSOCK-LISTEN:62001,fork,reuseaddr "UNIX-CONNECT:$pcscd_sock" &
-    _pcscd_vsock_relay_pid=$!
-  fi
-}
-
-stop_pcscd_vsock_relay() {
-  if [[ -n "$_pcscd_vsock_relay_pid" ]]; then
-    kill "$_pcscd_vsock_relay_pid" 2>/dev/null || true
-    _pcscd_vsock_relay_pid=""
-  fi
-}
